04fb28686f6e56e689379f2c4427f7dd0d99e47b
Sirling <mrsirling@gmail.com>
2020-01-02 16:58:14 +0200

    base structure for project

--- Python/db_SQLAlchemy/rawSql/test.py
+++ Python/db_SQLAlchemy/rawSql/test.py
@@ -4,10 +4,10 @@ from Python.db_SQLAlchemy.rawSql import queries
 
 def dk():
     db = Database()
-    # db.select(query=queries.select_vacancy)
-    # db.update(query=queries.update_vacancies)
-    # db.select(query=queries.select_vacancy)
-    # db.update(query=queries.insert_vacancy, )
+    # DB.select(query=queries.select_vacancy)
+    # DB.update(query=queries.update_vacancies)
+    # DB.select(query=queries.select_vacancy)
+    # DB.update(query=queries.insert_vacancy, )
     db.select(query=queries.select_inserted_vacancies)
 
 dk()
